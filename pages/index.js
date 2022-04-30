f5c769f43538899874a1cc5ca13087ec3913ab21
Jordan Hilado <jordanalihilado@gmail.com>
2022-04-30 09:55:15 -0700

    update index

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to StudyBuddy!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Your favorite study group finder.
         </p>
 
         <div className={styles.grid}>
